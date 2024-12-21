6ae994a9f5de44c678e2227318eb2bda1dd84040
MEXPOUER <aryaansubhasisray@gmail.com>
2024-12-21 13:17:47 -0500

    added the main clock and implemented ETC feature

--- planner.cpp
+++ planner.cpp
@@ -1,9 +1,11 @@
 # include<iostream>
 # include<chrono>
 # include<fstream>
+//"thread" allows you to pass off certain parts of this program to different threads in the CPU, or just stop the thread/ process entirely
+# include <thread>
 
 using namespace std;
-
+using namespace std::chrono;
 class Task{
     string taskName;
     string date;
@@ -23,15 +25,29 @@ class Task{
 
 //how to use date and time in c++
 class Timer{
+    public:
     string timerName;
     int timeMinutes;
     float ETC;
+    //duration is an "interval of time" type and "time point" is a point in time, two different things
+    void setTimer(int mins, system_clock::time_point timePoint)
+    {
+        //works now!
+        auto newTime = system_clock::to_time_t(timePoint + minutes(mins));
+        cout<<"ETC: " << ctime(&newTime);
+    }
 };
 
 int main()
 {
+    Timer timer;
+    //system clock is a static member class of chrono that has all the stuff like "now()" and time conversion from epoch time to human readable time
     auto timePoint = chrono::system_clock::now();
-    auto time = chrono::system_clock::to_time_t(timePoint);
+    auto time = system_clock::to_time_t(timePoint);
 
-    cout<< time;
+    timer.setTimer(10, timePoint);
+    
+    cout<< ctime(&time);
+    //"stops" the thread for 1 second, converted to one second via the chrono library, which knows what 1 second is in computer clock speed
+    this_thread::sleep_for(chrono::seconds(1));
 }
