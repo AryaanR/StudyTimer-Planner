b9a8afa8e4c98c94a0aaa8c27a4281eb49a01b1b
MEXPOUER <aryaansubhasisray@gmail.com>
2024-12-21 21:30:12 -0500

    Updated Command Line UI

--- Overview.md
+++ Overview.md
@@ -10,3 +10,25 @@ Nice to Have:
 - Reminder notifications
 - "Forget me not" Tasks (daily stuff you keep on forgetting, like exercising or brushing)
 - AI-connected habit help (how do i make this habit stick?)
+
+
+
+UI (Command Line MVP)
+    /*
+    1. Tasks
+    2. Timers
+    3. Quit
+    
+    if Tasks
+    1. Load All Tasks
+    2. Create a new task
+    3. Save Task
+
+    if Timers
+    1. Work Timer
+
+    in work Timer mode...
+    1. Pause (update ETC every minute)
+    2. Create Sub Timer
+
+    */
--- planner.cpp
+++ planner.cpp
@@ -54,35 +54,37 @@ class Task{
     }
 };
 
+class Clock{
+    public: 
+    //make this a process in another thread to stop it from stopping program
+    static void printTime()
+    {
+        while (true)
+        {
+            //system clock is a static member class of chrono that has all the stuff like "now()" and time conversion from epoch time to human readable time
+            auto timePoint = chrono::system_clock::now();
+            auto time = system_clock::to_time_t(timePoint);
+            cout<< ctime(&time) << flush;
+            //"stops" the thread for 1 second, converted to one second via the chrono library, which knows what 1 second is in computer clock speed
+            this_thread::sleep_for(chrono::seconds(5));
+        }
+    }
+};
 //how to use date and time in c++
 class Timer{
     public:
-    string timerName;
     int secondsTime;
     int minutesTime;
     int hoursTime;
-    float ETC;
-    //duration is an "interval of time" type and "time point" is a point in time, two different things
-    void setTimer(int mins, system_clock::time_point timePoint)
-    {
-        //works now!
-        auto newTime = system_clock::to_time_t(timePoint + hours(hoursTime) 
-        + minutes(minutesTime)
-        + seconds(secondsTime));
-        cout<<"ETC: " << ctime(&newTime);
-    }
 
-    Timer (string initTimerName)
+    Timer()
     {
-        timerName = initTimerName;
-
-        cout << "Timer: " << timerName << endl << " Duration (hours mins seconds): ";
+        cout << "Timer Duration (hours mins seconds): ";
         cin >> hoursTime >> minutesTime >> secondsTime;
     }
 
-    Timer (string initTimerName, int initHours, int initMinutes, int initSeconds)
+    Timer(int initHours, int initMinutes, int initSeconds)
     {
-        timerName = initTimerName;
         secondsTime = initSeconds;
         minutesTime = initMinutes;
         hoursTime = initHours;
@@ -93,21 +95,60 @@ class pomodoroTimer : Timer{
 
     Timer workTimer;
     Timer breakTimer;
+    float ETC;
+    //duration is an "interval of time" type and "time point" is a point in time, two different things
+    void setTimer(int mins, system_clock::time_point timePoint)
+    {
+        auto newTime = system_clock::to_time_t(timePoint + hours(hoursTime) 
+        + minutes(minutesTime)
+        + seconds(secondsTime));
+        cout<<"ETC: " << ctime(&newTime);
+    }
+};
 
+
+class SubTimer : Timer{
+    string timerName;
+
+    SubTimer (string initTimerName, int initHours, int initMinutes, int initSeconds)
+    : Timer(initHours, initMinutes, initSeconds)
+    {
+
+    }
 };
 
 int main()
 {
-    Task* newTask = new Task("my task");
-    newTask->printTask();
-    Timer* newTimer = new Timer("newTimer");
-    //system clock is a static member class of chrono that has all the stuff like "now()" and time conversion from epoch time to human readable time
-    auto timePoint = chrono::system_clock::now();
-    auto time = system_clock::to_time_t(timePoint);
+    cout<< "===================================================================="<< endl;
+    cout<< "                     T A S K  M A S T E R  VER. 0.1                "<<endl;
+    cout<< "===================================================================="<< endl;
+    //UI
+    cout << "Choose an option: " <<endl;
+    cout << "1. Tasks" << endl << "2. Timers" << endl << "3. Quit" << endl;
+    string input;
+    cin >> input; 
+    int intInput = stoi(input);
+    while (intInput > 3 || intInput < 1)
+    {
+        cout << "Please choose a valid option" << endl;
+        cin >> input; 
+        intInput = stoi(input);
+        cout<< intInput;
+    }
+    if (intInput == 1)
+    {
+        //Tasks Menu
+    }
 
-    newTimer->setTimer(10, timePoint);
-    
-    cout<< ctime(&time);
-    //"stops" the thread for 1 second, converted to one second via the chrono library, which knows what 1 second is in computer clock speed
-    this_thread::sleep_for(chrono::seconds(1));
+    if (intInput == 2)
+    {
+        //Timers Menu
+    }
+
+    if (intInput == 3)
+    {
+        //quit
+        cout<<"Quitting...";
+        return 0;
+    }
 }
